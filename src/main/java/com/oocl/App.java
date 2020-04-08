121272343838d6553de4fe9d997516d9026d33b2
Mavis Chan <mavis.chan@oocl.com>
2020-04-08 22:55:37 +0800

    [Mavis] - Passed test case should_return_value_for_correct_number_but_wrong_position

--- src/main/java/com/oocl/App.java
+++ src/main/java/com/oocl/App.java
@@ -1,5 +1,8 @@
 package com.oocl;
 
+import java.io.Serializable;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -10,33 +13,36 @@ public class App
 {
     private String randomNumber = "1234";
     private int aValue = 0;
+    private int bValue = 0;
+    private String result = "";
     String randomFirsDigit = randomNumber.substring(0,1);
     String randomSeconDigit = randomNumber.substring(1,2);
     String randomThirDigit = randomNumber.substring(2,3);
     String randomLasDigit =  randomNumber.substring(3,4);
 
-    public int checkCorrectNumberAndPostition(String inputValue) {
+    public String checkCorrectNumberAndPosition(String inputValue) {
         String input1Digit = inputValue.substring(0,1);
         String input2Digit = inputValue.substring(1,2);
         String input3Digit = inputValue.substring(2,3);
         String input4Digit =  inputValue.substring(3,4);
-        String[] inputNumberArray = {input1Digit,input2Digit,input3Digit,input4Digit};
-        System.out.print(Arrays.toString(inputNumberArray));
+        ArrayList<String> inputNumberArray = new ArrayList<String>(Arrays.asList(input1Digit,input2Digit,input3Digit,input4Digit));
+        System.out.print(inputNumberArray.toString());
 
-        String[] randomNumberArray = {randomFirsDigit,randomSeconDigit,randomThirDigit,randomLasDigit};
-        System.out.print(Arrays.toString(randomNumberArray));
+        ArrayList<String> randomNumberArray = new ArrayList<String>(Arrays.asList(randomFirsDigit,randomSeconDigit,randomThirDigit,randomLasDigit));
+        System.out.print(randomNumberArray.toString());
 
 
-        for (int i = 0; i <= inputNumberArray.length; i++) {
-            if (inputNumberArray[i].equals(randomNumberArray[i])) {
+        for (int i = 0; i < inputNumberArray.size(); i++) {
+            if (inputNumberArray.get(i).equals(randomNumberArray.get(i))) {
                 aValue += 1;
                 System.out.println(aValue);
             } else {
-              aValue += 0;
+                if(randomNumberArray.contains(inputNumberArray.get(i))) {
+                    bValue += 1;
+                }
             }
-            return aValue;
         }
-        System.out.println(aValue);
-        return aValue;
+        result = aValue + "A" + bValue + "B";
+        return result;
     }
 }
--- src/test/java/com/oocl/AppTest.java
+++ src/test/java/com/oocl/AppTest.java
@@ -17,13 +17,20 @@ public class AppTest {
     public void should_return_value_for_all_correct_number_and_correct_position() {
         App app = new App();
         String inputValue = "1234";
-        Assert.assertEquals(4,app.checkCorrectNumberAndPostition(inputValue));
+        Assert.assertEquals("4A0B",app.checkCorrectNumberAndPosition(inputValue));
     }
 
     @Test
     public void should_return_value_for_partial_correct_number() {
         App app = new App();
         String inputValue = "1567";
-        Assert.assertEquals(1, app.checkCorrectNumberAndPostition(inputValue));
+        Assert.assertEquals("1A0B", app.checkCorrectNumberAndPosition(inputValue));
+    }
+
+    @Test
+    public void should_return_value_for_correct_number_but_wrong_position() {
+        App app = new App();
+        String inputValue = "0324";
+        Assert.assertEquals("1A2B", app.checkCorrectNumberAndPosition(inputValue));
     }
 }
