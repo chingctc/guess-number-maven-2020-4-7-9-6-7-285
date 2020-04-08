4f7e704fe7bc193b5bec60b781043fae0f655a4f
Mavis Chan <mavis.chan@oocl.com>
2020-04-08 20:25:18 +0800

    [Mavis] - Passed test case should_return_value_for_all_correct_number_and_correct_position

--- src/main/java/com/oocl/App.java
+++ src/main/java/com/oocl/App.java
@@ -1,13 +1,35 @@
 package com.oocl;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private String randomNumber = "1234";
+    private int aValue;
+    String randomFirsDigit = randomNumber.substring(0,1);
+    String randomSeconDigit = randomNumber.substring(1,2);
+    String randomThirDigit = randomNumber.substring(2,3);
+    String randomLasDigit =  randomNumber.substring(3,4);
+
+    public int correctNumberButWrongPosition(String inputValue) {
+        String inputFirsDigit = inputValue.substring(0,1);
+        String inputSeconDigit = inputValue.substring(1,2);
+        String inputThirDigit = inputValue.substring(2,3);
+        String inputLasDigit =  inputValue.substring(3,4);
+        String[] inputNumberArray = {inputFirsDigit,inputSeconDigit,inputThirDigit,inputLasDigit};
+        System.out.print(Arrays.toString(inputNumberArray));
+
+        String[] randomNumberArray = {randomFirsDigit,randomSeconDigit,randomThirDigit,randomLasDigit};
+        System.out.print(Arrays.toString(randomNumberArray));
+        System.out.print(Arrays.equals(inputNumberArray,randomNumberArray));
+        if(Arrays.equals(inputNumberArray,randomNumberArray)) {
+            this.aValue = 4;
+        };
+        return aValue;
     }
+
 }
--- src/test/java/com/oocl/AppTest.java
+++ src/test/java/com/oocl/AppTest.java
@@ -1,10 +1,22 @@
 package com.oocl;
 
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import org.junit.Assert;
 import org.junit.Test;
 
-public class AppTest {
-    @Test
-    public void helloWorld() {
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+import java.io.IOException;
 
+import static javafx.beans.binding.Bindings.when;
+
+public class AppTest {
+
+    @Test
+    public void should_return_value_for_all_correct_number_and_correct_position() {
+        App app = new App();
+        String inputValue = "1234";
+        Assert.assertEquals(4,app.correctNumberButWrongPosition(inputValue));
     }
 }
